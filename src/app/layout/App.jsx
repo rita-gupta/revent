79c74a88ff3a27982ed8fbac11f65d1c0b0228bc
rita gupta <ritagupta1014@gmail.com>
2020-09-24 13:56:34 +0545

    project started

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+// import './app/layout/styles.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>welcome back</h1>
+    </div>
+  );
+}
+
+export default App;
